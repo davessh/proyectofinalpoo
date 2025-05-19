e1ec549dabb1c4115909676b4f8e6cbc89e6e7ab
davessh <david.schlemmer@uabc.edu.mx>
2025-05-19 10:36:00 -0700

    clase jugador con nueva logica de all in y apuesta

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,39 +1,79 @@
+import java.util.ArrayList;
+
+/**
+ * Clase que representa un jugador de poker
+ */
 public class Jugador {
     private String nombre;
-    private Mano mano;
     private int dinero;
+    private Mano mano;
     private boolean activo;
-
+    private boolean allIn;
 
     public Jugador(String nombre, int dineroInicial) {
         this.nombre = nombre;
         this.dinero = dineroInicial;
+        this.mano = null;
         this.activo = true;
+        this.allIn = false;
+    }
+
+    public void apostar(int cantidad) {
+        if (cantidad > dinero) {
+            cantidad = dinero;
+            allIn = true;
+        }
+        this.dinero -= cantidad;
+        if (dinero == 0) {
+            allIn = true;
+        }
+    }
+
+    public void recibir(int cantidad) {
+        this.dinero += cantidad;
+    }
+
+    public void retirarse() {
+        this.activo = false;
+    }
+
+    public boolean estaActivo() {
+        return activo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getDinero() {
+        return dinero;
+    }
+
+    public Mano getMano() {
+        return mano;
     }
 
     public void setMano(Mano mano) {
         this.mano = mano;
     }
 
-    public Mano getMano() {
-        return mano;
+    public boolean isAllIn() {
+        return allIn;
     }
-    public int getDinero() {
-        return dinero;
+
+    public void setAllIn(boolean allIn) {
+        this.allIn = allIn;
     }
-    public void apostar(int cantidad) {
-        dinero -= cantidad;
-    }
-    public void recibir(int cantidad) {
-        dinero += cantidad;
-    }
-    public void retirarse() {
-        activo = false;
-    }
-    public boolean estaActivo() {
-        return activo;
-    }
-    public String getNombre() {
-        return nombre;
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(nombre).append(" (").append(dinero).append(" fichas)");
+        if (allIn) {
+            sb.append(" [ALL-IN]");
+        } else if (!activo) {
+            sb.append(" [RETIRADO]");
+        }
+        return sb.toString();
     }
 }
